f255ee909817ec3c5c297bab57714f1e027cdf7c
Luciano Zavala <luczze@gmail.com>
2021-09-19 00:35:43 -0500

    first functionalities added

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# Index page
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('home.html')
+
+
+# List page
+@app.route('/list')
+def list_records():
+    return render_template('list.html')
+
+
+# Add new secret agent page
+@app.route('/enternew')
+def enter_new_agent():
+    return render_template('enternew.html')
+
+
+# error in the query landing page
+@app.route('/addrec')
+def wrong_query():
+    return 'wrong query'
 
 
 if __name__ == '__main__':
